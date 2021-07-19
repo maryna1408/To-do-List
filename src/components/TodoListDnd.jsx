c9bedb52abb5d762577a7460880b11e9a03bc418
maryna1408 <cabanmarina9@gmail.com>
2021-07-19 16:03:56 +0300

    DND fixes

--- src/components/TodoListDnd.jsx
+++ src/components/TodoListDnd.jsx
@@ -1,7 +1,7 @@
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Todo from "./Todo";
 import { useState, useEffect } from "react";
-import {  updateTodo } from "../api/crud";
+import { updateTodo } from "../api/crud";
 import { useTodos } from "./../hooks/useTodos";
 import { determineStatus, termineStatus } from "./../utils/common";
 
@@ -13,13 +13,11 @@ export default function TodoListDnd() {
     done: [],
   });
   useEffect(() => {
-    console.log('todos from context',todos);
+    console.log("todos from context", todos);
     const statuses = [1, 2, 3];
     const newState = {};
     statuses.forEach((status) => {
-      const filteredTodos = todos
-        .filter((todo) => todo.status === status)
-        .sort((a, b) => a.priority - b.priority || b.updatedAt - a.updatedAt || b.createdAt - a.createdAt);
+      const filteredTodos = todos.filter((todo) => todo.status === status);
       newState[determineStatus(status)] = filteredTodos;
     });
     setTodoLists(newState);
@@ -36,32 +34,29 @@ export default function TodoListDnd() {
   //       return result;
   //     }
 
-  const move =  (sourceList, destinationList, droppableSource, droppableDestination) => {
+  async function move(sourceList, destinationList, droppableSource, droppableDestination) {
     const sourceClone = [...sourceList];
     const destClone = [...destinationList];
     const [removed] = sourceClone.splice(droppableSource.index, 1);
     //   destClone.splice(droppableDestination.index, 0, removed);
-    const newStatus = termineStatus(droppableDestination.droppableId)
-    const newTodoData = [
-      removed.status = newStatus,
-    removed.updatedAt = Date.now()
-    ]
-    const [updatedTodo, updatedTodoError] = updateTodo(droppableSource.index, newTodoData);
+    const newStatus = termineStatus(droppableDestination.droppableId);
+    removed.status = newStatus;
+    removed.updatedAt = Date.now();
+    destClone.push(removed);
+    setTodoLists((prev) => ({
+      ...prev,
+      [droppableSource.droppableId]: sourceClone,
+      [droppableDestination.droppableId]: destClone,
+    }));
+    const [updatedTodo, updatedTodoError] = await updateTodo(removed.id, removed);
     if (!updatedTodoError) {
-      
       dispatchTodos({ type: "UPDATE", payload: updatedTodo });
-      destClone.push(removed);
-    const newState = {};
-    newState[droppableSource.droppableId] = sourceClone;
-    newState[droppableDestination.droppableId] = destClone;
-    return newState;
     } else {
       alert("Error! Todo is not updated! Try again later...");
     }
-    
-  };
+  }
 
-  function onDragEnd({ source, destination }) {
+  async function onDragEnd({ source, destination }) {
     // dropped outside the list
     if (!destination) {
       return;
@@ -70,77 +65,88 @@ export default function TodoListDnd() {
     //     const items = reorder();
     //   }
     if (source.droppableId !== destination.droppableId) {
-      const result = move(todoLists[source.droppableId], todoLists[destination.droppableId], source, destination);
-      const newState = { ...todoLists, ...result };
-      dispatchTodos({ type: "INIT", payload: Object.values(newState).flat() });
+      move(todoLists[source.droppableId], todoLists[destination.droppableId], source, destination);
     }
   }
   return (
     <div className="todos-wrapper">
       <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="new" direction="vertical">
-          {(provided) => (
-            <div className="todos new" ref={provided.innerRef}>
-              <h2 className="status">New</h2>
-              {todoLists.new.map((todo, index) => {
-                return (
-                  <Draggable key={todo.id.toString()} draggableId={todo.id.toString()} index={index}>
-                    {(provided) => {
-                      return (
-                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          <Todo todo={todo}></Todo>
-                        </div>
-                      );
-                    }}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        <Droppable droppableId="process" direction="vertical">
-          {(provided) => (
-            <div className="todos process" ref={provided.innerRef}>
-              <h2 className="status">Process</h2>
-              {todoLists.process.map((todo, index) => {
-                return (
-                  <Draggable key={todo.id.toString()} draggableId={todo.id.toString()} index={index}>
-                    {(provided) => {
-                      return (
-                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          <Todo todo={todo}></Todo>
-                        </div>
-                      );
-                    }}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        <Droppable droppableId="done" direction="vertical">
-          {(provided) => (
-            <div className="todos done" ref={provided.innerRef}>
-              <h2 className="status">Done</h2>
-              {todoLists.done.map((todo, index) => {
-                return (
-                  <Draggable key={todo.id.toString()} draggableId={todo.id.toString()} index={index}>
-                    {(provided) => {
-                      return (
-                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                          <Todo todo={todo}></Todo>
-                        </div>
-                      );
-                    }}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+        <div className="todos new">
+          <h2>new</h2>
+          <Droppable droppableId="new" direction="vertical">
+            {(provided) => (
+              <div className="todos-content" ref={provided.innerRef}>
+                {todoLists.new
+                  .sort((a, b) => a.priority - b.priority || b.updatedAt - a.updatedAt || b.createdAt - a.createdAt)
+                  .map((todo, index) => {
+                    return (
+                      <Draggable key={todo.id.toString()} draggableId={todo.id.toString()} index={index}>
+                        {(provided) => {
+                          return (
+                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <Todo todo={todo}></Todo>
+                            </div>
+                          );
+                        }}
+                      </Draggable>
+                    );
+                  })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+
+        <div className="todos process">
+          <h2>process</h2>
+          <Droppable droppableId="process" direction="vertical">
+            {(provided) => (
+              <div className="todos-content" ref={provided.innerRef}>
+                {todoLists.process
+                  .sort((a, b) => a.priority - b.priority || b.updatedAt - a.updatedAt || b.createdAt - a.createdAt)
+                  .map((todo, index) => {
+                    return (
+                      <Draggable key={todo.id.toString()} draggableId={todo.id.toString()} index={index}>
+                        {(provided) => {
+                          return (
+                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <Todo todo={todo}></Todo>
+                            </div>
+                          );
+                        }}
+                      </Draggable>
+                    );
+                  })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+        <div className="todos done">
+          <h2>done</h2>
+          <Droppable droppableId="done" direction="vertical">
+            {(provided) => (
+              <div className="todos-content" ref={provided.innerRef}>
+                {todoLists.done
+                  .sort((a, b) => a.priority - b.priority || b.updatedAt - a.updatedAt || b.createdAt - a.createdAt)
+                  .map((todo, index) => {
+                    return (
+                      <Draggable key={todo.id.toString()} draggableId={todo.id.toString()} index={index}>
+                        {(provided) => {
+                          return (
+                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <Todo todo={todo}></Todo>
+                            </div>
+                          );
+                        }}
+                      </Draggable>
+                    );
+                  })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
       </DragDropContext>
     </div>
     // <div className="todos-wrapper">
